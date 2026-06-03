2b3b6ab372177c154a4dee5c00fa3c2b6a15287e
jihanwinonajihan-cmyk <jihanwinonajihan@gmail.com>
2026-06-03 18:50:47 +0800

    login.php

--- login.php
+++ login.php
@@ -4,12 +4,11 @@ require_once 'config/database.php';
 
 if (isset($_SESSION['user_id'])) {
     $role = $_SESSION['role'];
-    if ($user['role'] === 'admin') header('Location: admin/dashboard.php');
-elseif ($user['role'] === 'dosen') header('Location: dosen/dashboard.php');
-else header('Location: mahasiswa/dashboard.php');
+    if ($role === 'admin') header('Location: dashboard.php');
+    elseif ($role === 'dosen') header('Location: dashboard.php');
+    else header('Location: dashboard.php');
     exit;
 }
-
 $error = '';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
